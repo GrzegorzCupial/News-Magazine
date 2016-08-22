8603b491eacac3592fd18ef00a277aac2804a8f1
GrzegorzCupial <grzegorzcupial@gmail.com>
2016-08-23 00:01:25 +0200

    Add files via upload
    #Added:
    - Contact & SinglePage
    - Fixed RWD
    - Fixed links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $(".button a").click(function(){
-        $(".overlay").fadeToggle(200);
+        $(".overlay").fadeToggle(600);
        $(this).toggleClass('btn-open').toggleClass('btn-close');
     });
 });
 $('.overlay').on('click', function(){
-    $(".overlay").fadeToggle(200);   
+    $(".overlay").fadeToggle(600);   
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
     open = false;
 });
