4705750a883833f4fef93fad31111df73756f6fc
Grzegorz Cupiał <grzegorzcupial@gmail.com>
2016-08-26 19:57:51 +0200

    Add files via upload
    Added:
    - Sticky Menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*HAMBURGER MENU*/
 $(document).ready(function(){
     $(".button a").click(function(){
         $(".overlay").fadeToggle(600);
@@ -8,4 +9,18 @@ $('.overlay').on('click', function(){
     $(".overlay").fadeToggle(600);   
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
     open = false;
+});
+
+
+/*STICKY MENU*/
+$("document").ready(function($){
+    var nav = $('#sticky-menu');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 225) {
+            nav.addClass("sticky-nav");
+        } else {
+            nav.removeClass("sticky-nav");
+        }
+    });
 });
